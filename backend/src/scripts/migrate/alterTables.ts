9d418e0958ecdf8f3d2946fac3ff7870c6024ff5
Pritam Thapa <57151837+pritamthapa2043@users.noreply.github.com>
2025-06-18 12:03:58 +0530

    B13---migrations (#17)
    * feat(tictactoe): change db file
    
    * feat(tictactoe): change packgae scripts

--- backend/src/scripts/migrate/alterTables.ts
+++ backend/src/scripts/migrate/alterTables.ts
@@ -1,11 +1,10 @@
 import { query } from "../../config/db";
 import { alterUserTable } from "../tables/core/userTable";
-import { alterTicTacToeGamesTable } from "../tables/tic_tac_toe/games";
 
 export const alterTables = async () => {
   try {
     await query(alterUserTable);
-    await query(alterTicTacToeGamesTable);
+
     console.log("All Alter table done successfully!");
   } catch (error: unknown | any) {
     console.error("❌ Error altering tables:", error.message);
