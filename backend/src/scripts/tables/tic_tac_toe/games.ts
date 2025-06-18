b2ecb322aca3ffc0639cbdb26fba80332618f907
pritamthapa2043 <pritam.thapa2043@gmail.com>
2025-06-18 12:02:12 +0530

    feat(tictactoe): change packgae scripts

--- backend/src/scripts/tables/tic_tac_toe/games.ts
+++ backend/src/scripts/tables/tic_tac_toe/games.ts
@@ -1,14 +1,10 @@
 export const createTicTacToeGames = `
 CREATE Table if not exists tic_tac_toe.games(
     id SERIAL PRIMARY KEY,
+    room_id varchar(10) NOT NULL;
     status TEXT NOT NULL CHECK (status IN ('waiting', 'in_progress', 'completed')),
     player_x_id INTEGER REFERENCES core.users(id),
     player_o_id INTEGER REFERENCES core.users(id),
     winner_id INTEGER REFERENCES core.users(id) ON DELETE SET NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );`;
-
-export const alterTicTacToeGamesTable = `
-Alter Table tic_tac_toe.games 
-add column room_id varchar(10) NOT NULL;
-`;
